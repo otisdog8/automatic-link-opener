32b452f7628da02c6f82ca63a777d104a1e09e9c
Otisdog8 <otis@otisroot.com>
2021-03-30 20:40:58 -0700

    Fixed install script
    Signed-off-by: Otisdog8 <otis@otisroot.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir ~/.config/link_opening/
 chmod +x opening/config_editor.py
 cp opening/daemon.py ~/.config/link_opening/daemon.py
 sudo cp opening/config_editor.py /usr/bin/config_editor
